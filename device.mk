db4230986d4bd21f8bc6c2bb44526bb9cca9b586
Łukasz Patron <priv.luk@gmail.com>
2024-06-23 20:41:44 +0700

    shark: Add ANT+ support
    Change-Id: I722da6672e91ee52e004eadf0e123f813db81d8e

--- device.mk
+++ device.mk
@@ -86,6 +86,10 @@ AB_OTA_POSTINSTALL_CONFIG += \
 PRODUCT_PACKAGES += \
     otapreopt_script
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
