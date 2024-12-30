cbd68298b767b378a25a4bce7b2b41d90a4b83ee
Łukasz Patron <priv.luk@gmail.com>
2024-12-30 23:20:37 +0700

    shark: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_shark.mk
+++ lineage_shark.mk
@@ -16,12 +16,8 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Black Shark
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_SYSTEM_NAME := shark
-
-BUILD_FINGERPRINT := "blackshark/SKR-A0/shark:10/G66X2011030CN00MQ4/V11.0.4.0.JOYUI:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="shark-user 10 G66X2011030CN00MQ4 V11.0.4.0.JOYUI release-keys" \
-    TARGET_PRODUCT="shark"
+    BuildDesc="shark-user 10 G66X2011030CN00MQ4 V11.0.4.0.JOYUI release-keys" \
+    BuildFingerprint=blackshark/SKR-A0/shark:10/G66X2011030CN00MQ4/V11.0.4.0.JOYUI:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
